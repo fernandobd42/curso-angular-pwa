9bd4bd50598f1a7d74ac11620433ed9aa202412a
Fernando Gontijo <fernandolucasbd@gmail.com>
2018-10-15 23:03:02 -0300

    construindo página de cadastro completa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MatInputModule,
+  MatButtonModule,
+} from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CardComponent } from './components/card/card.component';
 import { CadastrarComponent } from './pages/cadastrar/cadastrar.component';
 import { EntrarComponent } from './pages/entrar/entrar.component';
@@ -20,7 +25,10 @@ import { RecuperarSenhaComponent } from './pages/recuperar-senha/recuperar-senha
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/cadastrar/cadastrar.component.ts
+++ src/app/pages/cadastrar/cadastrar.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material';
+import swal from 'sweetalert2';
+
+import { MyErrorStateMatcher } from './../../utils/errorStateMatcher';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cadastrar',
@@ -6,10 +12,65 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cadastrar.component.scss']
 })
 export class CadastrarComponent implements OnInit {
+  cadastroForm: FormGroup;
+  matcher: ErrorStateMatcher = new MyErrorStateMatcher();
+  errorMsg: String = '';
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.inicialForm();
   }
 
+  inicialForm() {
+    this.cadastroForm = this.fb.group({
+      nome: ['',
+        [Validators.required, Validators.minLength(8)]
+      ],
+      email: ['',
+        [Validators.required, Validators.email]
+      ],
+      senha: ['',
+        [Validators.required, Validators.minLength(8)]
+      ],
+      confirmarSenha: ['',
+        [Validators.required, Validators.minLength(8)]
+    ]
+    });
+  }
+
+  get(atributo) {
+    return this.cadastroForm.controls[`${atributo}`];
+  }
+
+  cadastrar(form) {
+    this.errorMsg = '';
+
+    if (form.invalid) {
+      Object.values(form.controls).map((atributo: any) => {
+        atributo.markAsTouched();
+      });
+
+      return;
+    }
+
+    if (this.get('senha').value !== this.get('confirmarSenha').value) {
+      this.errorMsg = 'Confirmar senha deve ser igual a senha';
+      return;
+    }
+
+    swal({
+      type: 'success',
+      title: 'Cadastro Realizado com Sucesso',
+      text: `${form.value.nome} - ${form.value.email}`,
+      showConfirmButton: false,
+      timer: 4000,
+    });
+
+    this.router.navigate(['/entrar']);
+    console.log(form.value);
+  }
 }
